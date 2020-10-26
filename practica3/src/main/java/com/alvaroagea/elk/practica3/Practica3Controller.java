97cc7544f949c0b4ed37040471e21174eeea8814
Antoñito <antonioalcocer13@gmail.com>
2020-10-26 11:57:11 +0100

    dependencias 7.9.3

--- practica3/src/main/java/com/alvaroagea/elk/practica3/Practica3Controller.java
+++ practica3/src/main/java/com/alvaroagea/elk/practica3/Practica3Controller.java
@@ -22,55 +22,17 @@ final class Practica3Controller extends Controller {
     private final Logger logger = LogManager.getLogger();
     @Override
     void index(Message message) throws IOException {
-        IndexRequest indexRequest = new IndexRequest(Controller.MESSAGE_INDEX)
-                .source(Controller.AUTHOR_FIELD, message.getAuthor(),
-                        Controller.TIME_FIELD, message.getTime(),
-                        Controller.MESSAGE_FIELD, message.getMessage());
-
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        logger.debug(indexResponse.toString());
+        throw new UnsupportedOperationException();
     }
 
 
     @Override
     List<Message> searchMessage(String message) throws IOException {
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.query(QueryBuilders.matchQuery(Controller.MESSAGE_FIELD, message));
-        SearchRequest searchRequest = new SearchRequest(Controller.MESSAGE_INDEX)
-                .source(sourceBuilder);
-        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-        List<Message> messages = new LinkedList<>();
-        response.getHits().iterator().forEachRemaining(it -> {
-            Map<String, Object> source = it.getSourceAsMap();
-            messages.add(
-                    new Message(
-                            Instant.parse(source.get(Controller.TIME_FIELD).toString()),
-                            source.get(Controller.AUTHOR_FIELD).toString(),
-                            source.get(Controller.MESSAGE_FIELD).toString()
-                    )
-            );
-        });
-        return messages;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     List<Message> searchAuthor(String author) throws IOException {
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.query(QueryBuilders.wildcardQuery(Controller.AUTHOR_FIELD, author));
-        SearchRequest searchRequest = new SearchRequest(Controller.MESSAGE_INDEX)
-                .source(sourceBuilder);
-        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-        List<Message> messages = new LinkedList<>();
-        response.getHits().iterator().forEachRemaining(it -> {
-            Map<String, Object> source = it.getSourceAsMap();
-            messages.add(
-                    new Message(
-                            Instant.parse(source.get(Controller.TIME_FIELD).toString()),
-                            source.get(Controller.AUTHOR_FIELD).toString(),
-                            source.get(Controller.MESSAGE_FIELD).toString()
-                    )
-            );
-        });
-        return messages;
+        throw new UnsupportedOperationException();
     }
 }
